522e82a6c17bd97c31a3afd6bdf540558f845f39
John Cannon <john.cannon@sydney.edu.au>
2025-04-10 18:31:56 +1000

    Copy latest gplately/ptt from GPlates/gplately@7b267fd.
    The update from GPlately was done with:
      git remote add --no-tags gplately https://github.com/GPlates/gplately.git
      git fetch gplately master
      git checkout refs/remotes/gplately/master:gplately ptt
      git commit

--- ptt/__init__.py
+++ ptt/__init__.py
@@ -1,12 +1,38 @@
+#
+#    Copyright (C) 2024 The University of Sydney, Australia
+#
+#    This program is free software; you can redistribute it and/or modify it under
+#    the terms of the GNU General Public License, version 2, as published by
+#    the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful, but WITHOUT
+#    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#    for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+""""ptt" stands for Plate Tectonics Tools.
+
+This "ptt" module provides a collection of common plate tectonic functionality that researchers can use in their workflows. 
+It is primarily built on top of the pyGPlates Python library.
+
+"""
+
 from .documentation import install_documentation
-from . import utils
-from . import cleanup_topologies
-from . import convert_xy_to_gplates
-from . import continent_contours
-from . import remove_plate_rotations
-from . import resolve_topologies
-from . import ridge_spreading_rate
-from . import rotation_tools
-from . import separate_ridge_transform_segments
-from . import subduction_convergence
-from . import velocity_tools
+from . import (
+    cleanup_topologies,
+    continent_contours,
+    convert_xy_to_gplates,
+    remove_plate_rotations,
+    resolve_topologies,
+    ridge_spreading_rate,
+    rotation_tools,
+    separate_ridge_transform_segments,
+    subduction_convergence,
+    utils,
+    velocity_tools,
+)
